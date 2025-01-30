c1d95f019cf49ce9b78b2293b6451caadf04dc75
Paul Miller <paul@paul.lol>
2025-01-30 17:04:50 -0600

    add release workflow

---
+++ scripts/package-macos.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -e # Exit on error
+
+RELEASE_DIR="target/release"
+APP_DIR="$RELEASE_DIR/macos"
+APP_NAME="Harbor.app"
+DMG_NAME="harbor.dmg"
+DMG_DIR="$RELEASE_DIR/macos"
+
+# package dmg
+echo "Packing disk image..."
+ln -sf /Applications "$DMG_DIR/Applications"
+hdiutil create "$DMG_DIR/$DMG_NAME" -volname "Harbor" -fs HFS+ -srcfolder "$APP_DIR" -ov -format UDZO
+echo "✨ Packed '$APP_NAME' in '$DMG_DIR/$DMG_NAME'" 
