670faa930cbbe950845e3a78fa3033e615f5fb6a
Paul Miller <paul@paul.lol>
2025-02-04 06:53:42 -0600

    warn when opening links

--- harbor-ui/src/components/confirm_modal.rs
+++ harbor-ui/src/components/confirm_modal.rs
@@ -28,55 +28,61 @@ impl Default for ConfirmModalState {
 
 pub fn confirm_modal<'a>(
     content: Element<'a, Message>,
-    state: &'a ConfirmModalState,
+    state: Option<&'a ConfirmModalState>,
 ) -> Element<'a, Message> {
-    let modal_content = container(
-        column![
-            text(&state.title).size(24),
-            text(&state.description),
-            row![
-                h_small_button("Cancel", SvgIcon::SmallClose, false)
-                    .on_press((*state.cancel_action).clone()),
-                h_small_button(&state.confirm_button_text, SvgIcon::SmallCheck, false)
-                    .on_press((*state.confirm_action).clone()),
-            ]
-            .spacing(10)
-        ]
-        .spacing(20),
-    )
-    .width(400)
-    .padding(24)
-    .style(|theme: &Theme| container::Style {
-        background: Some(theme.palette().background.into()),
-        text_color: Some(theme.palette().text),
-        border: light_container_style(theme).border,
-        shadow: Shadow {
-            color: Color::from_rgba8(0, 0, 0, 0.5),
-            offset: Vector::new(4.0, 4.0),
-            blur_radius: 8.0,
-        },
-    });
+    let mut layers = stack![content];
 
-    stack![
-        content,
-        // This layer blocks all pointer events from reaching the content below
-        // TODO: can we do something cleaner?
-        button(container(text("")).width(Length::Fill).height(Length::Fill))
-            .on_press((*state.cancel_action).clone())
-            .style(|_theme: &Theme, _state| button::Style::default()),
-        container(center(modal_content))
-            .width(Length::Fill)
-            .height(Length::Fill)
-            .style(|_theme: &Theme| container::Style {
-                background: Some(
-                    Color {
-                        a: 0.8,
-                        ..Color::BLACK
-                    }
-                    .into()
-                ),
-                ..container::Style::default()
-            })
-    ]
-    .into()
+    if let Some(state) = state {
+        let modal_content = container(
+            column![
+                text(&state.title).size(24),
+                text(&state.description),
+                row![
+                    h_small_button("Cancel", SvgIcon::SmallClose, false)
+                        .on_press((*state.cancel_action).clone()),
+                    h_small_button(&state.confirm_button_text, SvgIcon::SmallCheck, false)
+                        .on_press((*state.confirm_action).clone()),
+                ]
+                .spacing(10)
+            ]
+            .spacing(20),
+        )
+        .width(400)
+        .padding(24)
+        .style(|theme: &Theme| container::Style {
+            background: Some(theme.palette().background.into()),
+            text_color: Some(theme.palette().text),
+            border: light_container_style(theme).border,
+            shadow: Shadow {
+                color: Color::from_rgba8(0, 0, 0, 0.5),
+                offset: Vector::new(4.0, 4.0),
+                blur_radius: 8.0,
+            },
+        });
+
+        // Add the overlay and modal layers
+        layers = layers.push(
+            // This layer blocks all pointer events from reaching the content below
+            button(container(text("")).width(Length::Fill).height(Length::Fill))
+                .on_press((*state.cancel_action).clone())
+                .style(|_theme: &Theme, _state| button::Style::default()),
+        );
+        layers = layers.push(
+            container(center(modal_content))
+                .width(Length::Fill)
+                .height(Length::Fill)
+                .style(|_theme: &Theme| container::Style {
+                    background: Some(
+                        Color {
+                            a: 0.8,
+                            ..Color::BLACK
+                        }
+                        .into(),
+                    ),
+                    ..container::Style::default()
+                }),
+        );
+    }
+
+    layers.into()
 }
