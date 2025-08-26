6cce7ca93c339fd77c0bdac1998bff223ebeb65b
Tommy Volk <tvolk131@gmail.com>
2025-08-25 22:32:46 -0500

    chore: enforce clippy match_bool rule

--- harbor-ui/src/main.rs
+++ harbor-ui/src/main.rs
@@ -8,7 +8,6 @@
     clippy::derive_partial_eq_without_eq,
     clippy::large_futures,
     clippy::manual_let_else,
-    clippy::match_bool,
     clippy::match_same_arms,
     clippy::match_wildcard_for_single_variants,
     clippy::missing_const_for_fn,
